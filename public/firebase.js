ef249ce10ed08b8a74c4dd8bff74ae3050b729a4
Okera Johnson <okerajohnson@gmail.com>
2022-02-16 10:16:58 -0500

    Add files via upload

---
+++ public/firebase.js
@@ -0,0 +1,15 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDG7ryBnx8vFt0N2G6sGqAdxyZt0NGAyCA",
+    authDomain: "habittracker-53909.firebaseapp.com",
+    projectId: "habittracker-53909",
+    storageBucket: "habittracker-53909.appspot.com",
+    messagingSenderId: "465326222384",
+    appId: "1:465326222384:web:855d0bdfec22586a06d1f5"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  console.log(firebase)
---
+++ public/script.js
@@ -0,0 +1,67 @@
+function showTime() {
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var session = "AM";
+
+    if (h == 0) {
+        h = 12;
+    }
+
+    if (h > 12) {
+        h = h - 12;
+        session = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+
+    var time = h + ":" + m;
+    //document.getElementById("MyClockDisplay").innerText = time;
+    //document.getElementById("MyClockDisplay").textContent = time;
+
+    setTimeout(showTime, 1000);
+
+}
+
+showTime();
+
+function myFunction() {
+    alert("I am an alert box!");
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+/*btn.onclick = function() {
+    modal.style.display = "block";
+} 
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+} */
+
+document.getElementById("submit").chrome.Action.addListener('click',function(){
+    chrome.browserAction.setPopup({
+        popup: './home.html'
+    });
+})
+      
+
+    
+
