c5cac49fd4df7bcf2749cf8afd99964c6e137a5c
Okera Johnson <okerajohnson@gmail.com>
2022-02-16 10:21:10 -0500

    Update firebase.js
    Removed the keys for safety

--- public/firebase.js
+++ public/firebase.js
@@ -1,12 +1,12 @@
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDG7ryBnx8vFt0N2G6sGqAdxyZt0NGAyCA",
+    apiKey: "<API KEY>",
     authDomain: "habittracker-53909.firebaseapp.com",
     projectId: "habittracker-53909",
     storageBucket: "habittracker-53909.appspot.com",
-    messagingSenderId: "465326222384",
-    appId: "1:465326222384:web:855d0bdfec22586a06d1f5"
+    messagingSenderId: "<messgID>",
+    appId: "<appID>"
   };
   
   // Initialize Firebase
